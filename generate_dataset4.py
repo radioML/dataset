ac3a3b43180ac6d03d80e17d0df1d04340d36b10
Nathan West <nathan.west@nrl.navy.mil>
2016-08-29 18:57:48 -0400

    go back to unity energy vectors

--- generate_dataset4.py
+++ generate_dataset4.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 from transmitters import transmitters
 from source_alphabet import source_alphabet
+import analyze_stats
 from gnuradio import channels, gr, blocks
 import numpy as np
 import numpy.fft, cPickle, gzip
@@ -17,19 +18,18 @@ dataset = {}
 # The output format looks like this
 # {('mod type', SNR): np.array(nvecs_per_key, 2, vec_length), etc}
 
-# CIFAR-10 has 10 classes and 60k samples. ~6k samples/vectors per class seems like right order of magnitude
-nvecs_per_key = 10000
+# CIFAR-10 has 6000 samples/class. CIFAR-100 has 600. Somewhere in there seems like right order of magnitude
+nvecs_per_key = 1000
 vec_length = 128
 snr_vals = range(-20,20,2)
 for snr in snr_vals:
+    print "snr is ", snr
     for alphabet_type in transmitters.keys():
-        print alphabet_type
         for i,mod_type in enumerate(transmitters[alphabet_type]):
           dataset[(mod_type.modname, snr)] = np.zeros([nvecs_per_key, 2, vec_length], dtype=np.float32)
           # moar vectors!
           insufficient_modsnr_vectors = True
           modvec_indx = 0
-          print "running test", i,mod_type
           while insufficient_modsnr_vectors:
               tx_len = int(10e3)
               if mod_type.modname == "QAM16":
@@ -57,12 +57,13 @@ for snr in snr_vals:
                   tb.connect(src, mod, snk)
               tb.run()
 
-              del src
               raw_output_vector = np.array(snk.data(), dtype=np.complex64)
               # start the sampler some random time after channel model transients (arbitrary values here)
               sampler_indx = random.randint(50, 500)
               while sampler_indx + vec_length < len(raw_output_vector) and modvec_indx < nvecs_per_key:
                   sampled_vector = raw_output_vector[sampler_indx:sampler_indx+vec_length]
+                  energy = np.sum((np.abs(sampled_vector)))
+                  sampled_vector = sampled_vector / energy
                   dataset[(mod_type.modname, snr)][modvec_indx,0,:] = np.real(sampled_vector)
                   dataset[(mod_type.modname, snr)][modvec_indx,1,:] = np.imag(sampled_vector)
                   # bound the upper end very high so it's likely we get multiple passes through
@@ -71,7 +72,6 @@ for snr in snr_vals:
                   modvec_indx += 1
 
               if modvec_indx == nvecs_per_key:
-                  print "we have enough modvecs"
                   # we're all done
                   insufficient_modsnr_vectors = False
                   # TODO: do a np permuter to shuffle here ?
